5022f92894efb88c53a7a2d815629f55395fb0ee
Hannah <hannahk8@illinois.edu>
2025-11-12 16:42:39 -0600

    Make 'ReportingConfirmation' fully interactive and operatable and fix layout of 'ReportComplete ' so that users can only move forward in the app

--- app/src/main/java/edu/illinois/cs465/uxtraordinary/ReportComplete.java
+++ app/src/main/java/edu/illinois/cs465/uxtraordinary/ReportComplete.java
@@ -1,6 +1,10 @@
 package edu.illinois.cs465.uxtraordinary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ReportComplete extends AppCompatActivity {
+public class ReportComplete extends AppCompatActivity
+    implements View.OnClickListener {
+
+
+
+
+    private Button endButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_report_complete);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        // The user has pressed the 'End' Button
+        endButton = (Button) findViewById(R.id.end_button_report_complete);
+        endButton.setOnClickListener(this);
+    }
+
+    // For when the user presses the 'Back' Button or 'End' Button
+    public void onClick(View v) {
+        // If the user presses, the 'Next' Button, go to
+        // the next reporting screen to select a BUILDING, FLOOR
+        if (v.getId() == R.id.end_button_report_complete) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
